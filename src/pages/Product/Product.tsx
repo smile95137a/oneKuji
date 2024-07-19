461a65611ce04bbc1119fc05ddc30353d821040d
jim <>
2024-07-19 22:23:00 +0800

    no message

--- src/pages/Product/Product.tsx
+++ src/pages/Product/Product.tsx
@@ -2,19 +2,8 @@ import Card from '@/components/MCard';
 import ProductCard from '@/components/ProductCard';
 import React from 'react';
 import pImg from '@/assets/image/p.png';
+import { FaFilter } from 'react-icons/fa';
 const products1 = [
-  {
-    imagePath: pImg,
-    imgStatus: '開抽中',
-    balanceText: '剩餘100抽',
-    money: '250',
-    unitIcon: '金',
-    unitText: '/抽',
-    title: '《我的英雄學院》~闖入~(日版)',
-    content:
-      '台灣最人氣【一番賞】線上抽選網站與日本0時差同步開抽！即時、方便、公開！台灣最人氣【一番賞】線上抽選網站與日本0時差同步開抽！即時、方便、公開！台灣最人氣【一番賞】線上抽選網站與日本0時差同步開抽！即時...',
-    customClass: '',
-  },
   {
     imagePath: pImg,
     imgStatus: '開抽中',
@@ -106,7 +95,9 @@ const Product = () => {
             <div className="product__list">
               <div className="product__list-title">
                 <div className="product__text">官方一番賞</div>
-                <div className="product__list-filter">x</div>
+                <div className="product__list-filter">
+                  <FaFilter />
+                </div>
               </div>
 
               <div className="product__list-products">
